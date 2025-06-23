a22cd7fdee01ad94a6c3da167d1ccf8607998872
Abraham1439 <ab.abanto@duocuc.cl>
2025-06-22 22:36:02 -0400

    Se actualizo la version en el pom

--- agendamiento/src/main/java/com/techmant/agendamiento/controller/AgendamientoController.java
+++ agendamiento/src/main/java/com/techmant/agendamiento/controller/AgendamientoController.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -60,6 +62,7 @@ public class AgendamientoController {
 
 
     //Endpoint para eliminar una agenda mediante su ID 
+    @DeleteMapping("/{id}")
     public ResponseEntity<?> eliminarAgendamiento(@PathVariable Long id) {
         try {
             agendamientoService.eliminarAgendamiento(id);
@@ -73,6 +76,7 @@ public class AgendamientoController {
 
     
     //Endpoint para actualizar una agenda mediente su ID (Si es que se necesita):)
+    @PutMapping("/{id}")
     public ResponseEntity <Agendamiento> actualizarAgendamiento(@PathVariable Long id, @RequestBody Agendamiento agenda) {
         try {
             //Creamos un objeto para buscar el paciente que queremos modificar
--- agendamiento/src/main/java/com/techmant/agendamiento/model/Agendamiento.java
+++ agendamiento/src/main/java/com/techmant/agendamiento/model/Agendamiento.java
@@ -13,7 +13,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "Agenda")
+@Table(name = "Agendamiento")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
