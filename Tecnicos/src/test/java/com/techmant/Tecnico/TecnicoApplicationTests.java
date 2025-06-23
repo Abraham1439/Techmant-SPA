e8008cdf769f5b4c9fdf46b1fbf1171d5d050de4
sebastiangome <sea.gomez@duocuc.cl>
2025-06-23 18:25:18 -0400

    Add files via upload

---
+++ Tecnicos/src/test/java/com/techmant/Tecnico/TecnicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techmant.Tecnico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TecnicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
